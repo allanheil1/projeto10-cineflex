2f19adb73747c2babea3b03050d1235c103847f6
allanheil1 <allanheil1@gmail.com>
2022-12-10 16:10:03 -0300

    Implementação lógica de acentos reservados

--- src/pages/SelecionarAssentos/SelecionarAssentos.js
+++ src/pages/SelecionarAssentos/SelecionarAssentos.js
@@ -7,6 +7,7 @@ export default function SelecionarAssentos(){
 
     const { sessionId } = useParams();
     const [assentos, setAssentos] = useState({});
+    let [reservados, setReservados] = useState([]);
 
     const promise = axios.get(`https://mock-api.driven.com.br/api/v8/cineflex/showtimes/${sessionId}/seats`);
 
@@ -14,8 +15,22 @@ export default function SelecionarAssentos(){
         {promise.then((res) => (setAssentos(res.data), console.log(res.data.seats)))}
     ,[])
 
-    function SelecionarAssentos(){
-        alert('assento selecionado');
+    function SelecionarAssento(id, isAvailable){
+        let novoReservados = reservados;
+
+        if(!isAvailable){
+            alert('Esse assento não está disponível');
+        }else if (novoReservados.includes(id)){
+            //caso já esteja na lista de reservados, removemos o item usando o filter:
+            novoReservados = novoReservados.filter(item => item !== id);
+            console.log(novoReservados);
+        }else{
+            novoReservados = [...novoReservados, id];
+            console.log(novoReservados);
+        }
+
+        setReservados(novoReservados);
+
     }
 
     return(
@@ -24,13 +39,18 @@ export default function SelecionarAssentos(){
             <List>
                 {assentos.seats ?
                     assentos.seats.map((a) => (
-                        <Assento key={a.id} isAvailable={a.isAvailable} onClick={SelecionarAssentos}> 
+                        <Assento key={a.id} isAvailable={a.isAvailable} onClick={() => SelecionarAssento(a.id, a.isAvailable)}> 
                             {a.name} 
                         </Assento>
                     ))
                     : 'Loading...'
                 }
             </List>
+            <ColorCaptions>
+                <Caption color={'#1AAE9E'}> <p> Selecionado </p>  </Caption>
+                <Caption color={'#C3CFD9'}> <p> Disponível </p> </Caption>
+                <Caption color={'#FBE192'}> <p> Indisponível </p> </Caption>
+            </ColorCaptions>
         </>
     )
 }
@@ -69,4 +89,35 @@ const Assento = styled.div`
   font-size: 11px;
   margin-bottom: 18px;
   cursor: pointer;
+`
+
+const ColorCaptions = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+const Caption = styled.div`
+  width: 22px;
+  margin: 4px 4px;
+  height: 22px;
+  border: 1px solid #808F9D;
+  background-color: ${props => (props.color)};
+  border-radius: 50%;
+  color: #000000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p{
+    margin-top: 50px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 15px;
+    display: flex;
+    align-items: center;
+    letter-spacing: -0.013em;
+    color: #4E5A65;
+  }
 `
